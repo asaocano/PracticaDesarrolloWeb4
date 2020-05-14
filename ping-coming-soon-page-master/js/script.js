d7669a8477be62a8066e0cc0113869afa80bee18
asaocano <manuelocano9@gmail.com>
2020-05-14 16:35:12 -0600

    Validación de correo

--- ping-coming-soon-page-master/js/script.js
+++ ping-coming-soon-page-master/js/script.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
+  $("#button").on("click", console.log("click"));
   $("#correo").on("blur", function () {
-    if ($("#correo").val().indexOf("@") < 0 || $("#correo").val() === "") {
+    if (
+      $("#correo").val().indexOf("@") < 0 ||
+      $("#correo").val() === "" ||
+      $("#correo").val().indexOf(".") < 0
+    ) {
       $("#correo").css({ border: "1px solid red" });
+      $(".error").show();
     } else {
       $("#correo").css({ border: "1px solid #5381F3" });
+      $(".error").hide();
     }
   });
 });
